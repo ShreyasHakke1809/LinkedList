ea4366a25d15f293913a0a9c756c7ef886a64a2e
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-25 17:06:23 +0530

    Adding simple linked list program

---
+++ DSLinkedList/CustomLinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSLinkedList
+{
+    internal class CustomLinkedList
+    {
+        public Node head;
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+                Console.WriteLine("{0} is inserted into lastnode", newNode.data);
+            }
+            else
+            {
+                Node lastNode = GetLastNode();
+                lastNode.next = newNode;
+                Console.WriteLine("{0} is inserted into lastnode", newNode.data);
+            }
+        }
+        private Node GetLastNode()
+        {
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            return temp;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Displaying Nodes:");
+            Node temp = head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- DSLinkedList/Program.cs
+++ DSLinkedList/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LinkedList Program");
+            CustomLinkedList customLinkedList = new CustomLinkedList();
+            customLinkedList.AddLast(56);
+            customLinkedList.AddLast(30);
+            customLinkedList.AddLast(70);
+            customLinkedList.Display();
         }
     }
 }
