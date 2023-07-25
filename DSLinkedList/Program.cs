30b0d4bb931113e4ae50cf98800d33e2dfbca759
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-25 17:01:37 +0530

    Adding welcome massage to main

---
+++ DSLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DSLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList Program");
+        }
+    }
+}
